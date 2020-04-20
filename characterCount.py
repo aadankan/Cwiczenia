d7b5e1d5fb5c0e40cd8de0c96a85ea3f1573f218
aadankan <aadankan@gmail.com>
2020-04-20 17:26:47 +0200

    characterCount.py-poprawka_2

--- characterCount.py
+++ characterCount.py
@@ -1,7 +1,10 @@
+from pprint import *
+
 message = 'It was a bright, cold April day and the clocks beat thirteenth'
 count = {}
+
 for character in message:
     count.setdefault(character, 0)
     count[character] = count[character] + 1
 
-print(count)
+pprint(count)
