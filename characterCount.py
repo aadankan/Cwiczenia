af5059d9b391dd4efa9ec265390021493dbe71ad
aadankan <aadankan@gmail.com>
2020-04-20 17:20:10 +0200

    characterCount.py

---
+++ characterCount.py
@@ -0,0 +1,7 @@
+message = 'IIt was a bright, cold April day and the clocks beat thirteenth'
+count = {}
+for character in message:
+    count.setdefault(character, 0)
+    count[character] = count[character] + 1
+
+print(count)
