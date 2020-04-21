d12131aad27df69f3851eed811a76dde8b709fa2
aadankan <aadankan@gmail.com>
2020-04-22 01:13:06 +0200

    isPhoneNumber2.py-wykrywanie-wielu-numerow

--- isPhoneNumber2.py
+++ isPhoneNumber2.py
@@ -2,11 +2,15 @@ import re, pyperclip
 
 phoneNumRegex1 = re.compile(r'\d\d\d \d\d\d \d\d\d')
 phoneNumRegex2 = re.compile(r'\d\d \d\d\d \d\d \d\d')
-mo1 = phoneNumRegex1.search(pyperclip.paste())
-mo2 = phoneNumRegex2.search(pyperclip.paste())
+mo1 = phoneNumRegex1.findall(pyperclip.paste())
+mo2 = phoneNumRegex2.findall(pyperclip.paste())
 if mo1 != None:
-    print('Znaleziono numer telefonu: ' + mo1.group())
-elif mo2 != None:
-    print('Znaleziono numer telefonu: ' + mo2.group())
+    for i in range(len(mo1)):
+        print('Znaleziono numer telefonu: ' + mo1[i])
+
+if mo2 != None:
+    for i in range(len(mo2)):
+        print('Znaleziono numer telefonu: ' + mo2[i])
+
 else:
     print('Nie znaleziono numeru')
