a7a95661c565a48579fd00b80c5d0b0cd935a243
aadankan <aadankan@gmail.com>
2020-04-21 23:32:51 +0200

    isPhoneNumber2

--- isPhoneNumber2.py
+++ isPhoneNumber2.py
@@ -1,5 +1,12 @@
-import re
+import re, pyperclip
 
-phoneNumRegex = re.compile(r'\d\d\d \d\d\d \d\d\d')
-mo = phoneNumRegex.search('Moj numer telefonu to 413 334 345')
-print('Znaleziono numer telefonu: ' + mo.group())
+phoneNumRegex1 = re.compile(r'\d\d\d \d\d\d \d\d\d')
+phoneNumRegex2 = re.compile(r'\d\d \d\d\d \d\d \d\d')
+mo1 = phoneNumRegex1.search(pyperclip.paste())
+mo2 = phoneNumRegex2.search(pyperclip.paste())
+if mo1 != None:
+    print('Znaleziono numer telefonu: ' + mo1.group())
+elif mo2 != None:
+    print('Znaleziono numer telefonu: ' + mo2.group())
+else:
+    print('Nie znaleziono numeru')
