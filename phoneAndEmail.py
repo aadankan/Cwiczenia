9db6a02d5291195dabe4322197c5b2182649a8b3
aadankan <aadankan@gmail.com>
2020-04-23 15:31:23 +0200

    phoneAndEmail.py-cz.3

--- phoneAndEmail.py
+++ phoneAndEmail.py
@@ -23,6 +23,15 @@ emailRegex = re.compile(r"""(
     (\.[a-zA-Z]{2,4})               # Kropka i pozniej cokolwiek
     )""", re.VERBOSE)
 
-# TODO: Wyszukanie dopasowan w schowku
+# Wyszukanie dopasowan w schowku
+text = str(pyperclip.paste())
+matches = []
+for groups in phoneRegex.findall(text):
+    phoneNum = '-'.join([groups[1], groups[3], groups[5]])
+    if groups[8] != '':
+        phoneNum += ' x' + groups[8]
+    matches.append(phoneNum)
+for groups in emailRegex.findall(text):
+    matches.append(groups[0])
 
 # TODO: Skopiowanie wynikow do schowka
