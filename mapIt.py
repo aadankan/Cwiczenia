bb7d7333f4849a222f952e6eeeea3284a9b76d46
aadankan <aadankan@gmail.com>
2020-04-27 19:23:50 +0200

    mapIt.py

---
+++ mapIt.py
@@ -0,0 +1,14 @@
+#! python3
+# mapIt.py-Wyswietla w przegladarce WWW mape na podstawie adresu
+# podanego w wierszu polecen lub w schowku
+
+import webbrowser, sys, pyperclip
+if len(sys.argv) > 1:
+    # Pobranie adresu z wiersza polecen
+    address = ' '.join(sys.argv[1:])
+
+else:
+    # Pobranie adresu ze schowka
+    address = pyperclip.paste()
+
+webbrowser.open('https://www.google.com/maps/place' + address)
