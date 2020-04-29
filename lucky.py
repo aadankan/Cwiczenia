24387d08c142c10872a146ca3c9b5ea5bda463f0
aadankan <aadankan@gmail.com>
2020-04-29 21:10:35 +0200

    lucky.py-cz.3

--- lucky.py
+++ lucky.py
@@ -1,7 +1,7 @@
 #! python3
 # lucky.py-Otwieramy kilka wynikow wyszukiwania Google
 
-import requests, sys, webbrowser, bs4
+import requests, sys, webbrowser, bs4, pyperclip
 
 print('Wyszukiwanie...')    # Komunikat wyswietlany podczas pobierania strony Google
 res = requests.get('https://www.google.com/search?q=' + ' '.join(sys.argv[1:]))
@@ -11,5 +11,13 @@ res.raise_for_status()
 soup = bs4.BeautifulSoup(res.text)
 
 # Otworzenie karty przegladarki WWW dla kazdego wyniku wyszukiwania
-linkElems = soup.select('.r a')
+linkElems = soup.select('.kCrYT a')
+numOpen = min(5, len(linkElems))
+for i in range(numOpen):
+    webbrowser.open('http://google.pl' + linkElems[i].get('href'))
+
+print('cos')
+print(linkElems)
+
+
 
