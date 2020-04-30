ba0ee31825bafe0126183a4f55707ed2dd746272
aadankan <aadankan@gmail.com>
2020-04-30 14:19:43 +0200

    downloadXkcd.py-cz.3

--- downloadXkcd.py
+++ downloadXkcd.py
@@ -13,9 +13,15 @@ while not url.endswith('#'):
 
     soup = bs4.BeautifulSoup(res.text)
 
-    # TODO: Ustalenie adresu URL pliku obrazu komiksu
-
-    # TODO: Pobranie obrazu
+    # Ustalenie adresu URL pliku obrazu komiksu
+    comicElem = soup.select('#comic img')
+    if comicElem == []:
+        print('Nie udalo sie odnalezc pliku obrazu komisksu')
+    else:
+        comicUrl = 'http:' + comicElem[0].get('src')
+        # Pobranie obrazu
+        print('Pobieranie obrazu %s...' % comicUrl)
+        res.raise_for_status()
 
     # TODO: Zapis obrazu w katalogu ./xkcd.
 
