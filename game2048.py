6f288f3f9febb5de6eb12292e59d5cf0d0e7164b
aadankan <aadankan@gmail.com>
2020-05-01 16:13:23 +0200

    game2048.py-2

--- game2048.py
+++ game2048.py
@@ -5,14 +5,14 @@ browser = webdriver.Chrome()
 browser.get('https://gabrielecirulli.github.io/2048/')
 
 html = browser.find_element_by_tag_name('html')
-for i in range(100):
-    for j in range(1000):
-        html.send_keys(Keys.UP)
-        html.send_keys(Keys.RIGHT)
-        html.send_keys(Keys.DOWN)
-        html.send_keys(Keys.LEFT)
-        try:
-            retry = browser.find_element_by_class_name('retry-button')
-            retry.click()
-        except:
-            continue
+
+while True:
+    html.send_keys(Keys.UP)
+    html.send_keys(Keys.RIGHT)
+    html.send_keys(Keys.DOWN)
+    html.send_keys(Keys.LEFT)
+    try:
+        retry = browser.find_element_by_class_name('retry-button')
+        retry.click()
+    except:
+        continue
