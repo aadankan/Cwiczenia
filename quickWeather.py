448563a42940908c2075b5e0b8119f5cf5247061
aadankan <aadankan@gmail.com>
2020-05-02 14:30:13 +0200

    quickWeather.py-cz.2

--- quickWeather.py
+++ quickWeather.py
@@ -9,6 +9,10 @@ if len(sys.argv) < 2:
     sys.exit()
 location = ' '.join(sys.argv[1:])
 
-# TODO: Pobranie danych w formacie JSON z API witryny OpenWeatheMap.org
+# Pobranie danych w formacie JSON z API witryny OpenWeatheMap.org
+url = 'htt://api.openweathermap.org/data/2.5/forecast/daile?q=%s&cnt=3' % location
+response = requests.get(url)
+response.raise_for_status()
 
 # TODO: Umieszczenie danych JSON w zmiennej Pythona
+
