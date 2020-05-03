bd724886e5ccedbfb930c2ed50f3ef27d7f8a0b3
aadankan <aadankan@gmail.com>
2020-05-03 19:50:42 +0200

    stopwatch.py-cz.2

--- stopwatch.py
+++ stopwatch.py
@@ -6,10 +6,21 @@ import time
 # Wyswietlanie inforamcji o sposobie dzialania programu
 print('Nacisnij Enter, aby rozpoczac. Kolejne nacisniecie Enter oznacza nowe okrazenie. Nacisniecie Ctrl+C '
       'konczy dzialanie programu')
-input()                     # Nacisnij Enter, aby rozpoczac
+input()  # Nacisnij Enter, aby rozpoczac
 print('Rozpoczeto dzialanie.')
-startime = time.time()      # Pobranie godziny rozpoczecia pierwszego okrazenia
+startime = time.time()  # Pobranie godziny rozpoczecia pierwszego okrazenia
 lastTime = startime
 lapNum = 1
 
-# TODO: Rozpoczecie pomiaru czasu okrazenia
+# Rozpoczecie pomiaru czasu okrazenia
+try:
+    while True:
+        input()
+        lapTime = round(time.time() - lastTime, 2)
+        totalTime = round(time.time() - startime, 2)
+        print('Okrazenie #%s: %s (%s)' % (lapNum, totalTime, lapTime), end='')
+        lapNum += 1
+        lastTime = time.time()  # Wyzerowanie czasu ostatniego okrazenia
+except KeyboardInterrupt:
+    # Obsluga wyjatku zglaszanego po nacisnieciu klawiszy Ctrl+C. Dzieki temu nie bedzie wyswietlony komunikat bledu
+    print('\nGotowe')
