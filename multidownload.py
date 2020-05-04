a0380aa1116f981a1923e922bafe1bf411a9b3ea
aadankan <aadankan@gmail.com>
2020-05-04 18:42:11 +0200

    multidownload.py-cz.3-poprawki

--- multidownload.py
+++ multidownload.py
@@ -2,7 +2,7 @@
 # multidownload.py-Pobiera wszystkie komiksy opublikowane w witrynie XKCD, uzywajac do tego wielu watkow
 
 import requests, os, bs4, threading
-os.makedirs('xkcd,', exist_ok=True)
+os.makedirs('xkcd', exist_ok=True)
 
 def downloadXkcd(startComic, endComic):
     for urlNumber in range(startComic, endComic):
@@ -18,9 +18,12 @@ def downloadXkcd(startComic, endComic):
         if comicElem == []:
             print('Nie udalo sie odnalezc pliku obrazu komiksu')
         else:
-            comicUrl = comicElem[0].get('src')
+            comicUrl = str(comicElem[0].get('src'))
+            comicUrl = 'http:' + comicUrl
+
             # Pobranie obrazu
             print('Pobieranie obrazu %s...' % comicUrl)
+            res = requests.get(comicUrl)
             res.raise_for_status()
 
             # Zapis obrazu w katalogu ./xkcd
@@ -31,7 +34,7 @@ def downloadXkcd(startComic, endComic):
 
 # Utworzenie i uruchomienie obiektow Thread
 downloadThreads = []
-for i in range(00, 1400, 100)
+for i in range(0, 2300, 100):
     downloadThread = threading.Thread(target=downloadXkcd, args=(i, i+99))
     downloadThreads.append(downloadThread)
     downloadThread.start()
