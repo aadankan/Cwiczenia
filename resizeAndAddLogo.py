3cd345fc7f4b6a5e65e3a6eac40bab3c13f2271c
aadankan <aadankan@gmail.com>
2020-05-11 00:09:42 +0200

    resizeAndAddLogo.py-cz.4

--- resizeAndAddLogo.py
+++ resizeAndAddLogo.py
@@ -1,6 +1,7 @@
 #! python3
 # resizeAndAddLogo.py-Zmiana wielkosci wszystkich obrazow w biezacym katalogu roboczym aby miescily sie w
 # kwadracie o wymiarach 300 x 300 pikseli. Nastepnie w prawym dolnym rogu bedzie umieszczone logo catlogo.png
+
 import os
 from PIL import Image
 
@@ -10,10 +11,11 @@ LOGO_FILENAME = 'logo.png'
 logoIm = Image.open(LOGO_FILENAME)
 logoWidth, logoHeight = logoIm.size
 
+
 os.makedirs('withLogo', exist_ok=True)
 # Iteracja przez wszystkie pliki w biezacym katalogu roboczym
 for filename in os.listdir('.'):
-    if not (filename.endswith('.png') or filename.endswith('.jpg')):
+    if not (filename.endswith('.png') or filename.endswith('.jpg')) or filename == LOGO_FILENAME:
         continue
 
     im = Image.open(filename)
@@ -33,6 +35,9 @@ for filename in os.listdir('.'):
         print('Zmiana wielkosci obrazu %s...' % filename)
         im = im.resize((width, height))
 
-# TODO: Dodanie logo
+    # Dodanie logo
+    print('Dodanie logo do obrazu %s...' % filename)
+    im.paste(logoIm, (width - logoWidth, height - logoHeight), logoIm)
 
-# TODO: Zapis wprowadzonych zmian
+    # Zapis wprowadzonych zmian
+    im.save(os.path.join('withLogo', filename))
