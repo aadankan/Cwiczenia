3615637dfbf898737ed836edc7be54eda66cb14b
aadankan <aadankan@gmail.com>
2020-05-11 00:10:53 +0200

    resizeAndAddLogo.py-cz.4-poprawka

--- resizeAndAddLogo.py
+++ resizeAndAddLogo.py
@@ -6,38 +6,38 @@ import os
 from PIL import Image
 
 SQUARE_FIT_SIZE = 300
-LOGO_FILENAME = 'logo.png'
+LOGO_FILENAME = 'catlogo.png'
 
 logoIm = Image.open(LOGO_FILENAME)
 logoWidth, logoHeight = logoIm.size
 
-
 os.makedirs('withLogo', exist_ok=True)
-# Iteracja przez wszystkie pliki w biezacym katalogu roboczym
+# Iteracja przez wszystkie pliki w bieżącym katalogu roboczym.
 for filename in os.listdir('.'):
-    if not (filename.endswith('.png') or filename.endswith('.jpg')) or filename == LOGO_FILENAME:
-        continue
+    if not (filename.endswith('.png') or filename.endswith('.jpg')) \
+       or filename == LOGO_FILENAME:
+        continue # Pominięcie plików innych niż obrazy oraz obrazu o nazwie pliku takiej samej jak logo.
 
     im = Image.open(filename)
     width, height = im.size
 
-    # Sprawdzanie, czy konieczna jest zmiana wielkosci obrazu
+    # Sprawdzenie, czy konieczna jest zmiana wielkości obrazu.
     if width > SQUARE_FIT_SIZE and height > SQUARE_FIT_SIZE:
-        # Obliczenie nowej szerokosci i wysokosci obrazu
+        # Obliczenie nowej szerokości i wysokości obrazu.
         if width > height:
-            height = int((SQUARE_FIT_SIZE/width) * height)
+            height = int((SQUARE_FIT_SIZE / width) * height)
             width = SQUARE_FIT_SIZE
         else:
-            width = int((SQUARE_FIT_SIZE / height) + width)
+            width = int((SQUARE_FIT_SIZE / height) * width)
             height = SQUARE_FIT_SIZE
 
-        #  Zmiana wielkosci obrazu
-        print('Zmiana wielkosci obrazu %s...' % filename)
+        # Zmiana wielkości obrazu.
+        print('Zmiana wielkości obrazu %s...' % (filename))
         im = im.resize((width, height))
 
-    # Dodanie logo
-    print('Dodanie logo do obrazu %s...' % filename)
+    # Dodanie logo.
+    print('Dodanie logo do obrazu %s...' % (filename))
     im.paste(logoIm, (width - logoWidth, height - logoHeight), logoIm)
 
-    # Zapis wprowadzonych zmian
+    # Zapis wprowadzonych zmian.
     im.save(os.path.join('withLogo', filename))
