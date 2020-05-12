a7a1a20c27e4f6763c0caf146c35feba5781aba0
aadankan <aadankan@gmail.com>
2020-05-12 20:39:38 +0200

    mouseNow.py-cz.1

---
+++ mouseNow.py
@@ -0,0 +1,5 @@
+#! python3
+# mouseNow.py-Wyswietla biezace polozenie kursora myszy
+import pyautogui
+print('Nacisnij klawisze Ctrl+C, aby zakonczyc dzialanie programu.')
+# TODO: Pobranie i wyswietlenie wspolrzednych kursora myszy
