86fed7c2f70e3a66181bab959cc7f11e4d6e9384
aadankan <aadankan@gmail.com>
2020-05-12 22:54:29 +0200

    mouseNow.py-v2

--- mouseNow.py
+++ mouseNow.py
@@ -7,6 +7,10 @@ try:
         # Pobranie i wyswietlenie wspolrzednych kursora myszy
         x, y = pyautogui.position()
         positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+        pixelColor = pyautogui.screenshot().getpixel((x,y))
+        positionStr += ' RGB: (' + str(pixelColor[0]).rjust(3)
+        positionStr += ', ' + str(pixelColor[1]).rjust(3)
+        positionStr += ', ' + str(pixelColor[2]).rjust(3) + ')'
         print(positionStr, end='')
         print('\b' * len(positionStr), end='', flush=True)
 
