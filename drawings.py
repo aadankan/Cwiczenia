20dd109f5e49979aaa6fce8990d35c9c096a7be0
aadankan <aadankan@gmail.com>
2020-05-14 16:07:28 +0200

    drawings.py-2

--- drawings.py
+++ drawings.py
@@ -16,24 +16,25 @@ class Drawing:
 
     def trojkat(self, x, h, wtime):
         time.sleep(self)
-        pyautogui.dragRel(x*2, 0, duration=wtime)
+        pyautogui.dragRel(x * 2, 0, duration=wtime)
         pyautogui.dragRel(-x, -h, duration=wtime)
         pyautogui.dragRel(-x, h, duration=wtime)
 
-    def kolo(self, r, wtime):
+    def kolo(self, r, d=1, wtime=0):
         time.sleep(self)
-        l = 2 * pi * r
-        y = 0
-
-
+        i = 0
+        a, b = pyautogui.position()
+        while i <= 2 * pi:
+            x = round(sin(i) * r)
+            y = round(cos(i) * r)
+            i += 1 / r * d
+            pyautogui.click(a+x, b-y, duration=wtime)
 
     def dom(self, x, y, h, wtime):
         time.sleep(self)
         Drawing.prostokat(0, x, y, wtime)
-        pyautogui.moveRel(-1/2*x, 0)
+        pyautogui.moveRel(-1 / 2 * x, 0)
         Drawing.trojkat(0, x, h, wtime)
-
-Drawing.kolo(4, 100, 0.5)
 
 
 
