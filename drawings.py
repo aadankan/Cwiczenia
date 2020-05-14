b5c0ed80c443f83e0d2b52b553554b004bbee287
aadankan <aadankan@gmail.com>
2020-05-14 16:32:33 +0200

    drawings.py-3

--- drawings.py
+++ drawings.py
@@ -7,20 +7,20 @@ class Drawing:
     def __init__(self):
         pass
 
-    def prostokat(self, x, y, wtime):
+    def prostokat(self=2, x=100, y=100, wtime=0):
         time.sleep(self)
         pyautogui.dragRel(x, 0, duration=wtime)
-        pyautogui.dragRel(0, x, duration=wtime)
-        pyautogui.dragRel(-y, 0, duration=wtime)
+        pyautogui.dragRel(0, y, duration=wtime)
+        pyautogui.dragRel(-x, 0, duration=wtime)
         pyautogui.dragRel(0, -y, duration=wtime)
 
-    def trojkat(self, x, h, wtime):
+    def trojkat(self=2, x=100, h=50, wtime=0):
         time.sleep(self)
         pyautogui.dragRel(x * 2, 0, duration=wtime)
         pyautogui.dragRel(-x, -h, duration=wtime)
         pyautogui.dragRel(-x, h, duration=wtime)
 
-    def kolo(self, r, d=1, wtime=0):
+    def kolo(self=2, r=100, d=1, wtime=0):
         time.sleep(self)
         i = 0
         a, b = pyautogui.position()
@@ -30,11 +30,19 @@ class Drawing:
             i += 1 / r * d
             pyautogui.click(a+x, b-y, duration=wtime)
 
-    def dom(self, x, y, h, wtime):
+    def dom(self=2, x=100, y=100, h=50, wtime=0):
         time.sleep(self)
         Drawing.prostokat(0, x, y, wtime)
         pyautogui.moveRel(-1 / 2 * x, 0)
         Drawing.trojkat(0, x, h, wtime)
+        pyautogui.moveRel(3/5*x, 1/5*x)
+        Drawing.prostokat(0, x/5, x/5)
+        pyautogui.moveRel(3/5*x, 0)
+        Drawing.prostokat(0, x / 5, x / 5)
+        pyautogui.moveRel(-7/10*x, -1/5*x)
+        pyautogui.moveRel(2/5*x,3/5*y)
+        Drawing.prostokat(0, x/5, 12/30*y)
 
 
+Drawing.dom()
 
