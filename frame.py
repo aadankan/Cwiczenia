1b3bc5b6bf3f7ec8af653b90670c097f5cc91b96
aadankan <aadankan@gmail.com>
2020-06-22 16:17:04 +0200

    frame.py

---
+++ frame.py
@@ -0,0 +1,19 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+root = Tk()
+root.title('Hi!')
+root.iconbitmap(r'C:\Users\aadan\Desktop\2.ico')
+
+frame = LabelFrame(root, padx=50, pady=50)
+frame.pack(padx=10, pady=10)
+
+b = Button(frame, text="Don't click me!")
+b2 = Button(frame, text="... or here!")
+b.grid(row=0, column=0)
+b2.grid(row=1, column=1)
+
+
+
+
+root.mainloop()
