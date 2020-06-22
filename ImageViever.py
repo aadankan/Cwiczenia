ee09ef974504b60ff9c0e4aa745c750ecefd1d3b
aadankan <aadankan@gmail.com>
2020-06-22 15:35:03 +0200

    ImageViever.py-2

--- ImageViever.py
+++ ImageViever.py
@@ -14,6 +14,7 @@ my_img5 = ImageTk.PhotoImage(Image.open(r'C:\Users\aadan\Desktop\5.png'))
 
 image_list = [my_img1, my_img2, my_img3, my_img4, my_img5]
 
+status = Label(root, text='Image 1 of ' + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
 
 my_label = Label(image=my_img1)
 my_label.grid(row=0, column=0, columnspan=3)
@@ -36,6 +37,9 @@ def forward(image_number):
     button_back.grid(row=1, column=0)
     button_forward.grid(row=1, column=2)
 
+    status = Label(root, text='Image '+ str(image_number) +' of ' + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
+    status.grid(row=2, column=0, columnspan=3, sticky=W + E)
+
 
 def back(image_number):
     global my_label
@@ -54,6 +58,9 @@ def back(image_number):
     button_back.grid(row=1, column=0)
     button_forward.grid(row=1, column=2)
 
+    status = Label(root, text='Image ' + str(image_number) +' of '+ str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
+    status.grid(row=2, column=0, columnspan=3, sticky=W + E)
+
 
 button_back = Button(root, text="<<", command=back, state=DISABLED)
 button_exit = Button(root, text="Exit Program", command=root.quit)
@@ -62,7 +69,7 @@ button_forward = Button(root, text=">>", command=lambda: forward(2))
 
 button_back.grid(row=1, column=0)
 button_exit.grid(row=1, column=1)
-button_forward.grid(row=1, column=2)
-
+button_forward.grid(row=1, column=2, pady=10)
+status.grid(row=2, column=0, columnspan=3, sticky=W+E)
 
 root.mainloop()
