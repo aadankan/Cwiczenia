8d1b7dc2e5fd7b7494fdc2e125cfd7ecf2f3d362
aadankan <aadankan@gmail.com>
2020-06-24 16:21:09 +0200

    slidersTk.py

---
+++ slidersTk.py
@@ -0,0 +1,25 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+root = Tk()
+root.title('message')
+root.iconbitmap(r'C:\Users\aadan\Desktop\2.ico')
+root.geometry("400x400")
+
+vertical = Scale(root, from_=0, to=200)
+vertical.pack()
+
+def slide():
+    my_label = Label(root, text=horizontal.get()).pack()
+    root.geometry(str(horizontal.get()) + "x" + str(vertical.get()))
+
+horizontal = Scale(root, from_=0, to=400, orient=HORIZONTAL)
+horizontal.pack()
+
+my_label = Label(root, text=horizontal.get()).pack()
+
+
+
+my_btn = Button(root, text="Click Me!", command=slide).pack()
+
+mainloop()
